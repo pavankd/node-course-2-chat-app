0780fd0facb3b88df4878ac7df2083e40d6c1b0e
pavan kumar dasari <pavank.d@gmail.com>
2017-05-27 15:34:57 +0530

    add mustache template rendering

--- public/js/index.js
+++ public/js/index.js
@@ -8,10 +8,20 @@ socket.on('disconnect',function(){
 });
 socket.on('newMessage',function(message){
   var formattedTime = moment(message.createdAt).format('h:mm a');
-  console.log('new Message',message);
-  var li = jQuery('<li></li>');
-  li.text(`${message.from} ${formattedTime} : ${message.text}`);
-  jQuery('#messages').append(li);
+  var template = jQuery('#message-template').html();
+  var html = Mustache.render(template,{
+    text:message.text,
+    from : message.from,
+    createSt:formattedTime
+  });
+
+  jQuery('#messages').append(html);
+
+  // var formattedTime = moment(message.createdAt).format('h:mm a');
+  // console.log('new Message',message);
+  // var li = jQuery('<li></li>');
+  // li.text(`${message.from} ${formattedTime} : ${message.text}`);
+  // jQuery('#messages').append(li);
 });
 
 socket.on('newLocationMessage',function(message){
